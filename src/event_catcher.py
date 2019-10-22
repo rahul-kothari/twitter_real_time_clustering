4c0217767ed72296db01557f023eb0b9c4c7ee82
Rahul Kothari <rahul.kothari.201@gmail.com>
2019-10-22 11:29:15 +0100

    add python main, test streaming

--- src/event_catcher.py
+++ src/event_catcher.py
@@ -1,5 +1,5 @@
 from config import *
-from tweepy import Stream, OAuthHandler, API,Cursor
+from tweepy import Stream, OAuthHandler
 from tweepy.streaming import StreamListener
 import csv
 import json
@@ -15,24 +15,12 @@ class TwitterListener(StreamListener):
 
     def on_data(self, data):
         print(data)
-        # f = csv.writer(open("../data/tweets.csv", "a"))
-        csv_file = csv.writer(open('../data/tweets.csv', 'a'))
-        for tweet in tweepy.Cursor(api.search, q="#unitedAIRLINES", count=100,
-                                   lang="en",
-                                   since="2017-04-03").items():
-            print(tweet.created_at, tweet.text)
-            csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
-
+        
     def on_error(self, status):
         print(status)
 
 
 if __name__ == '__main__' :
-    #twitterStream = Stream(auth, TwitterListener())
-    #twitterStream.filter(track=["trump", "donald trump"])
-    api = API(auth)
-    csv_file = csv.writer(open('../data/tweets.csv', 'a'))
-    for tweet in Cursor(api.search, q="#trump", count=100,lang="en").items():
-        print(tweet.created_at, tweet.text)
-        csv_file.writerow([tweet.created_at, tweet.text.encode('utf-8')])
 
+    twitterStream = Stream(auth, TwitterListener())
+    twitterStream.filter(track=["trump", "donald trump"])
--- src/step1_twitter_scrapper.py
+++ src/step1_twitter_scrapper.py
@@ -1,12 +1,12 @@
 from config import *
-from tweepy import API, OAuthHandler, Cursor
+from tweepy import API, Cursor, AppAuthHandler
 from data_cleanup import remove_urls_users_punctuations
 
 
 def create_twitter_object():
     # OAuth process
-    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_API_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    auth = AppAuthHandler(CONSUMER_KEY, CONSUMER_API_SECRET)
+    #auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     # ceate tweepy api object -
     # tell it to wait how much ever needed in case we reached rate limit and notify me too
     api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
@@ -32,9 +32,9 @@ def get_tweets(num_of_tweets, search_criteria=TOPIC, lang="en"):
     return tweets
 
 
-if __name__ == "main":
+if __name__ == '__main__' :
     # get tweets and put it in a file
-    data = get_tweets(100)
+    data = get_tweets(NUM_OF_TWEETS_NEEDED)
     filename = FILE_PATH
     delimiter = "\n"
     open(filename, 'a', encoding="utf8").write(delimiter.join(data))
